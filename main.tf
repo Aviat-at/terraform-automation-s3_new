0dc72e4935b1dc794c1fd54870d0cd87f494c7c9
Aviat-at <akashtharindu7788@gmail.com>
2025-03-21 17:26:14 -0400

    first-commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "bucket_prefix" {
+  default = "akash-dev-storage"
+}
+
+resource "aws_s3_bucket" "buckets" {
+  count  = 4
+  bucket = "${var.bucket_prefix}-${count.index + 1}"
+
+  tags = {
+    Name = "${var.bucket_prefix}-${count.index + 1}"
+  }
+}
